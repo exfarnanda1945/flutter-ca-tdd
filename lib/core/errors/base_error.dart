9a687ab75bd5f39e76f700f392be8d2c80ab8c3a
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-12-03 22:57:51 +0700

    add: base error and api error

---
+++ lib/core/errors/base_error.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+// Abstract error for all error hapened
+abstract class BaseError extends Equatable {
+  const BaseError({required this.message, required this.statusCode});
+
+  final String message;
+  final String statusCode;
+
+  @override
+  List<Object> get props => [message, statusCode];
+}
