2c15a42ac0cc0168b25647802c749f7060691370
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-12-03 22:59:09 +0700

    add: user entity

---
+++ lib/src/auth_feat/domain/entities/user.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+// Equatable --> Compare between object (as a value not as an object instance) in flutter without overriding ==
+class User extends Equatable {
+  const User(
+      {required this.id,
+      required this.createdAt,
+      required this.name,
+      required this.avatar});
+
+  final int id;
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+// Compare between object and id as a key
+// Whenever the Id are same, thats a same person / user
+  @override
+  List<Object?> get props => [id];
+}
