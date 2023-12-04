09098ba8dc127258a65249e8a3a69b8ddede3bab
miko <exfar@samir.co.id>
2023-12-04 17:40:25 +0700

    add: auth repository mock test

---
+++ test/src/auth_feat/domain/usecases/auth_repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_flutter/src/auth_feat/domain/repositories/auth_repository.dart';
+
+class MockAuthRepository extends Mock implements AuthRepository {}
