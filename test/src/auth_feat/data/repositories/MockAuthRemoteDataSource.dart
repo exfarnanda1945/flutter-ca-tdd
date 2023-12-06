e77f6970e5748bae9ad3a25c7b261ea832ac8f4a
miko <exfar@samir.co.id>
2023-12-06 18:02:09 +0700

    add: mock auth remote data source for test

---
+++ test/src/auth_feat/data/repositories/MockAuthRemoteDataSource.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_flutter/src/auth_feat/data/data_source/remote/auth_remote_data_source.dart';
+
+class MockAuthRemoteDataSource extends Mock implements AuthRemoteDataSource{}
