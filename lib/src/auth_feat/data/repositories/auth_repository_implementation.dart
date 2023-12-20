471242f5d3de73f2aec292a3a098cdf7feeb09b5
miko <exfar@samir.co.id>
2023-12-20 17:11:15 +0700

    fix: trigger auth repo impl for test

--- lib/src/auth_feat/data/repositories/auth_repository_implementation.dart
+++ lib/src/auth_feat/data/repositories/auth_repository_implementation.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:tdd_flutter/core/errors/base_error.dart';
+import 'package:tdd_flutter/core/errors/server_error.dart';
+import 'package:tdd_flutter/core/exceptions/server_exception.dart';
 import 'package:tdd_flutter/core/utils/typedef.dart';
 import 'package:tdd_flutter/src/auth_feat/data/data_source/remote/auth_remote_data_source.dart';
 import 'package:tdd_flutter/src/auth_feat/domain/entities/user.dart';
@@ -17,14 +19,22 @@ class AuthRepositoryImplementation implements AuthRepository {
       {required String createdAt,
       required String name,
       required String avatar}) async {
-    await _remoteDataSource.createUser(
-        createdAt: createdAt, name: name, avatar: avatar);
-    return const Right(null);
+    try {
+      await _remoteDataSource.createUser(
+          createdAt: createdAt, name: name, avatar: avatar);
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerError.fromException(e));
+    }
   }
 
   @override
-  ResultFuture<BaseError, List<User>> listUser() {
-    // TODO: implement listUser
-    throw UnimplementedError();
+  ResultFuture<BaseError, List<User>> listUser() async {
+    try {
+      final result = await _remoteDataSource.listUser();
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerError.fromException(e));
+    }
   }
 }
